a93cecb077478a22eee6a7f22a8bb70c459cd398
ldasilva <ldasilva@eecs.wsu.edu>
2021-02-03 15:33:43 -0800

    Started assignment and added stylecop

--- HW1/HW1/Program.cs
+++ HW1/HW1/Program.cs
@@ -1,10 +1,20 @@
-﻿using System;
-
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 namespace HW1
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// Homework 1 Assignment.
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main function calls all.
+        /// </summary>
+        /// <param name="args"> main arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
