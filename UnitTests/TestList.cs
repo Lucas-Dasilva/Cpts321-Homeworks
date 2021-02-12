e98835b87d5e75e78790ab2c4be44744fbf12a40
ldasilva <ldasilva@eecs.wsu.edu>
2021-02-11 18:38:58 -0800

    Oops didn't include unittest project file in last commit

---
+++ UnitTests/TestList.cs
@@ -0,0 +1,23 @@
+﻿namespace HW2
+{
+    using NUnit.Framework;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    class TestList
+    {
+
+        [Test]
+        public void TestCreateList()
+        {
+            GenerateHash testClass = new GenerateHash();
+            List<int> testList = testClass.listGenerator();
+            // List<int> List = new List<int>() 
+
+            Assert.IsNotEmpty(testList);
+
+
+        }
+
+    }
+}
