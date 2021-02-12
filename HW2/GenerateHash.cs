b302ed653ca934702c8c0e104c6adb2f3e23f961
ldasilva <ldasilva@eecs.wsu.edu>
2021-02-12 15:53:50 -0800

    added a pseudo function for FilterList and Test for that function

--- HW2/GenerateHash.cs
+++ HW2/GenerateHash.cs
@@ -2,17 +2,33 @@
 {
     using System.Collections.Generic;
 
-
+    /// <summary>
+    /// Use the Random class to create a list with 10,000 random 
+    /// integers in the range[0, 20000]
+    /// Then determine how many distinct integers are in the list with 3 different approaches.
+    /// Also, have them run in the order listed below. Do not change the order.
+    /// </summary>
     public class GenerateHash
     {
         /// <summary>
         /// Generate list with 10,000 random ints 
         /// </summary>
         /// <returns>list of ints</returns>
-        public List<int> listGenerator()
+        public List<int> ListCreator()
         {
             List<int> randIntList = new List<int>();
             return randIntList;
+        }
+
+        /// <summary>
+        /// Filter out integer list and put only distinct integers inside hashset 
+        /// </summary>
+        /// <param name="randIntList"> list with 10k random integers</param>
+        /// <returns>Distinct integer list hashset</returns>
+        public HashSet<int> ListParser(List<int> randIntList)
+        {
+            HashSet<int> distinceIntSet = new HashSet<int>();
+            return distinceIntSet;
         }
         
     }
